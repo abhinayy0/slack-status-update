11a7f05d6a9f022774abf6f7fba6557839688ccc
Abhinay Yadav <abhinayy0@gmail.com>
2024-05-26 17:14:05 +0530

    updates slack app for oauth:

--- app.js
+++ app.js
@@ -1,14 +1,75 @@
 const { App } = require("@slack/bolt");
 
-const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./database.sqlite",
 });
 
-/* Add functionality here */
+const User = sequelize.define("user", {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: Sequelize.STRING,
+  },
+  accessToken: {
+    type: Sequelize.STRING,
+  },
+  teamId: {
+    type: Sequelize.STRING,
+  },
+  userId: {
+    type: Sequelize.STRING,
+  },
+  installation: {
+    type: Sequelize.STRING,
+  },
+});
 
+sequelize.sync();
+
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: "my-state-secret",
+  scopes: ["commands", "users:write"],
+  installerOptions: {
+    userScopes: [
+      "channels:write",
+      "channels:read",
+      "chat:write",
+      "im:read",
+      "users:write",
+      "users.profile:write",
+    ],
+    directInstall: true,
+  },
+  installationStore: {
+    storeInstallation: async (installation) => {
+      // change the line below so it saves to your database
+      return await User.create({
+        firstName: "janedoe",
+        installation: JSON.stringify(installation),
+        accessToken: installation.user.token,
+        userId: installation.user.id,
+        teamId: installation.team.id,
+      });
+    },
+    fetchInstallation: async (installQuery) => {
+      // change the line below so it fetches from your database
+      const user = await User.findOne({
+        where: { userId: installQuery.userId },
+      });
+      let jsonObject = JSON.parse(user.installation);
+      return jsonObject;
+    },
+  },
+});
 (async () => {
   // Start the app
   await app.start(process.env.PORT || 3000);
@@ -19,6 +80,8 @@ const app = new App({
     logger.info(context);
     await next();
   });
+
+  /* Add functionality here */
 
   app.command("/status-update", async ({ command, ack, respond }) => {
     // Acknowledge command request
